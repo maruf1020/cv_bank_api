90298e5d2f4f982def230eb62ce528d5df8f82a2
maruf1020 <maruf.billah@northsouth.edu>
2025-06-18 04:54:05 +0600

    update repository

---
+++ src/common/interfaces/paginated-result.interface.ts
@@ -0,0 +1,8 @@
+// src/common/interfaces/paginated-result.interface.ts
+export interface PaginatedResult<T> {
+  data: T[];
+  total: number;
+  page: number;
+  limit: number;
+  totalPages: number;
+}
